abdbb1066329ab497936c7e739b90dcde2586563
Debasish <debasish7044200115@gmail.com>
2021-03-29 11:55:58 +0530

    init

---
+++ app/config/config.php
@@ -0,0 +1,17 @@
+<?php 
+
+// DB params
+define('DB_HOST','localhost');
+define('DB_USER', 'root');
+define('DB_PASS','123456');
+define('DB_NAME','debMvc');
+
+// APP root
+define("APPROOT"  ,dirname(dirname(__FILE__)));
+
+// URL root
+define('URLROOT','http://localhost/debMvc');
+
+
+// SITE name
+define('SITENAME','DebasishMVC');
---
+++ app/controllers/Pages.php
@@ -0,0 +1,24 @@
+<?php
+
+class Pages extends Controller {
+   public function __construct()
+   {
+   
+     
+   }
+
+   public function index(){
+  
+     $data = [
+       'title' => "WELCOME",
+     ];
+      $this->view('pages/index', $data);
+   }
+
+   public function about(){
+     $data = [
+       'title' => "ABOUT US"
+     ];
+     $this->view('pages/about',$data);
+   }
+}
